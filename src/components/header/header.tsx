b77a569ae236097cbf82ec6f5119c56c25eca8f4
codewithalihamza <512syedalihamza@gmail.com>
2024-03-28 21:35:42 +0500

    updating background color of app bar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
   
     return (
         <div>
-            <AppBar position="static">
+            <AppBar position="static" className='app-bar-background'>
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
                         <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
